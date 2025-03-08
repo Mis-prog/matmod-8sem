262f0f97f50446bdf08a50244022d9941d074ee1
Акмурзин М Э <akmurzinmihail@gmail.com>
2025-03-08 19:39:10 +0500

    Добавил отрисовку и запись первой частицы

--- lab1/misha/src/main.cpp
+++ lab1/misha/src/main.cpp
@@ -39,8 +39,11 @@ int main() {
 
     MatrixXd init_vals(M, 2);
     init_vals.setZero();
-    std::ofstream out("../lab1/misha/result/calc.csv");
-    out << "<x>\t<y>\t<R>\t<x^2>\t<y^2>\t<Δx^2>\t<Δy^2>\t<ΔR^2>" << std::endl;
+
+    std::ofstream out_calc("../lab1/misha/result/calc.csv"), out_first_particle(
+            "../lab1/misha/result/first_particle.csv");
+    out_calc << "<x>\t<y>\t<R>\t<x^2>\t<y^2>\t<Δx^2>\t<Δy^2>\t<ΔR^2>" << std::endl;
+    out_first_particle << "x y" << endl;
 
     for (int i = 0; i < N; i++) {
         VectorXd curr_l(M);
@@ -51,17 +54,19 @@ int main() {
             // Генерация случайных направлений
             int dir = dir_distrib(gen);
             curr_dirs.row(j) = direction.row(dir);
-        }
-        MatrixXd temp_result = curr_l.asDiagonal() * curr_dirs.cast<double>();
 
-        init_vals += temp_result;
-        VectorXd result = calc(init_vals);
-        for (int k = 0; k < result.size(); k++) {
-            out << result[k] << "\t";
         }
-        out << endl;
+        init_vals += curr_l.asDiagonal() * curr_dirs.cast<double>();
+        VectorXd result = calc(init_vals);
+
+        for (int k = 0; k < result.size(); k++) {
+            out_calc << result[k] << "\t";
+        }
+        out_calc << endl;
+        out_first_particle << init_vals.row(0) << endl;
     }
 
-    out.close();
+    out_first_particle.close();
+    out_calc.close();
     return 0;
 }
