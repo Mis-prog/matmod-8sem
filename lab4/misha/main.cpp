01ad97a2654880328bada567d76004c54e02497b
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-03-22 14:47:28 +0500

    Начинаю писать разностную схему

---
+++ lab4/misha/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr int N = 100, K = 10;
+constexpr double density = 725.0, dynamic_viscosity = 0.53e-3;
+constexpr double nu = dynamic_viscosity / density;
+constexpr double v0_x = 2;
+
+int main()
+{
+    return 0;
+}
