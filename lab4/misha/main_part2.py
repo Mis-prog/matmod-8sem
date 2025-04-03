931694073af936c338dd15db376b064199663b36
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-04-03 11:10:32 +0500

    Изменения по 4 лабе(осталось добить 2 задание)

--- lab4/misha/main_part2.py
+++ lab4/misha/main_part2.py
@@ -45,42 +45,4 @@ plt.plot(x, funk(x, a_opt), color='black',ls='--', label=rf"Аппроксима
 plt.legend()
 plt.show()
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from scipy.integrate import solve_ivp
 
-# # Определяем систему уравнений
-# def system(x, y):
-#     f, fp, fpp, fppp = y  # y[0] = f, y[1] = f', y[2] = f'', y[3] = f'''
-#     return [fp, fpp, fppp, -f * fp / 2]
-
-# # Метод стрельбы
-# def shooting(s):
-#     y0 = [0, 0, s, 0]  # f(0)=0, f'(0)=0, f''(0)=s, f'''(0)=0
-#     sol = solve_ivp(system, (0, 10), y0, t_eval=np.linspace(0, 10, 100))
-#     return sol.t, sol.y[0], sol.y[1]  # x, f(x), f'(x)
-
-# # Подбор s методом дихотомии
-# s_min, s_max = -1, 1  # Диапазон начального условия f''(0)
-# tolerance = 1e-4
-
-# while s_max - s_min > tolerance:
-#     s_mid = (s_min + s_max) / 2
-#     _, _, f_prime = shooting(s_mid)
-#     if f_prime[-1] > 1:
-#         s_max = s_mid
-#     else:
-#         s_min = s_mid
-
-# # Решаем с оптимальным s
-# x, f, f_prime = shooting(s_mid)
-
-# # Построение графика
-# plt.plot(x, f, label="f(x)")
-# plt.plot(x, f_prime, label="f'(x)")
-# plt.axhline(1, linestyle="--", color="gray", label="f' → 1")
-# plt.xlabel("x")
-# plt.ylabel("f, f'")
-# plt.legend()
-# plt.grid()
-# plt.show()
---
+++ lab4/misha/main_part3.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+def blasius_eq(eta, y):
+    f, f_prime, f_double_prime = y
+    f_triple_prime = - (f * f_double_prime) / 2  # Исходное уравнение
+    return [f_prime, f_double_prime, f_triple_prime]
+
+def shoot(fpp0_guess, eta_max=10, n_points=100):
+    eta_vals = np.linspace(0, eta_max, n_points)
+    y0 = [0, 0, fpp0_guess]  # Начальные условия: f(0)=0, f'(0)=0, f''(0)=предположение
+    sol = solve_ivp(blasius_eq, [0, eta_max], y0, t_eval=eta_vals, method='RK45')
+    return sol.t, sol.y
+
+def find_optimal_fpp0(target_f_prime_inf=1, tol=1e-5, eta_max=10, n_points=100):
+    fpp0_low, fpp0_high = 0.1, 1.0  # Начальный диапазон для f''(0)
+    while fpp0_high - fpp0_low > tol:
+        fpp0_guess = (fpp0_low + fpp0_high) / 2
+        eta_vals, (f_vals, f_prime_vals, _) = shoot(fpp0_guess, eta_max, n_points)
+        
+        if f_prime_vals[-1] < target_f_prime_inf:
+            fpp0_low = fpp0_guess  # Недооценили f''(0), надо больше
+        else:
+            fpp0_high = fpp0_guess  # Переоценили, надо меньше
+    
+    return (fpp0_low + fpp0_high) / 2
+
+fpp0_opt = find_optimal_fpp0() 
+eta_vals, (f_vals, f_prime_vals, f_double_prime_vals) = shoot(fpp0_opt)
+
+nu = 0.53e-3 / 725     
+U_inf = 2
+
+Nx, Ny = 20000, 5000
+x_vals = np.linspace(0 , 2, Nx)  
+y_vals = []
+
+fou, Ly = 10, 1
+for j in range( Ny):
+    y_vals.append((fou ** (j * Ly / Ny  - Ly) )/ (fou - 1))
+y_vals = np.array(y_vals)
+
+X, Y = np.meshgrid(x_vals, y_vals)
+ETA = Y / np.sqrt(nu * X / U_inf + 1e-9)
+
+u = U_inf * np.interp(ETA, eta_vals, f_prime_vals) 
+v = 0.5 * np.sqrt(nu*U_inf/X) * (ETA *  np.interp(ETA,eta_vals,f_prime_vals) -  np.interp(ETA, eta_vals, f_vals))
+
+print(u[0])
