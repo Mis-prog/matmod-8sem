65421dbc1c88daa70a6d504a80f99fd65af70620
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-04-09 12:00:30 +0500

    Вроде сделал

--- lab3/misha/main_test.cpp
+++ lab3/misha/main_test.cpp
@@ -2,15 +2,14 @@
 #include "main_lib.h"
 
 
-int main()
-{
-    const int Nx = 400;
+int main() {
+    const int Nx = 1000;
     // const int Nl = 1000;
     const int Ny = 400;
-    const double L = 5.0;
-    const double Lpml = 3.0;
+    const double L = 10;
+    const double Lpml = 5;
     const double k = 10;
-    const double eps = 0;
+    const double eps = 1e-5;
     const double ynull = 0.5;
     LIB lib(Nx, Ny, L, Lpml, ynull, k, eps);
     lib.calc();
--- lab3/misha/report/draw.py
+++ lab3/misha/report/draw.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-data = np.loadtxt('../result/result10y00.50.txt')
+data = np.loadtxt('../result/result20y00.50.txt')
 Ny, Nx = data.shape
-x = np.linspace(0, 8, Nx)
+print(Ny, Nx)
+x = np.linspace(0, 15, Nx)
 y = np.linspace(0, 1, Ny)
 X, Y = np.meshgrid(x, y)
 
@@ -15,38 +16,20 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 plt.show()
 
+for i in range(Nx):
+    plt.plot(y, data[:, i], color='blue')
+# plt.plot(y, data[:, 100], color='blue')
+plt.xlabel('x')
+plt.ylabel('z')
+plt.grid(True)
+plt.title('Проекция на ZY')
+plt.show()
+
 for i in range(Ny):
     plt.plot(x, data[i, :], color='blue')
+# plt.plot(y, data[:, 100], color='blue')
 plt.xlabel('x')
 plt.ylabel('z')
 plt.grid(True)
 plt.title('Проекция на ZX')
 plt.show()
-
-# print(data[0, :])
-# y = data[:, 0]
-# x = data[:, 1]
-# values = data[:, 2]
-#
-# x_unique = np.unique(x)
-# y_unique = np.unique(y)
-#
-# X, Y = np.meshgrid(x_unique, y_unique)
-# Z = np.zeros_like(X)
-#
-# for i in range(len(x)):
-#     xi = np.where(x_unique == x[i])[0][0]  # Индекс x в сетке
-#     yi = np.where(y_unique == y[i])[0][0]  # Индекс y в сетке
-#     Z[yi, xi] = values[i]
-#
-# fig = plt.figure(figsize=(12, 8))  # Размер окна
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # Рисуем поверхность
-# surf = ax.plot_surface(X, Y, Z, cmap='plasma')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlim(0, 1)
-# ax.view_init(azim=225, elev=30)
-#
-# plt.show()
