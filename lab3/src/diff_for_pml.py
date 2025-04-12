1df9c9efd12246c2ea9baf095d48079a163dd1d9
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-04-13 01:51:47 +0500

    Так вроде гуд

--- lab3/src/diff_for_pml.py
+++ lab3/src/diff_for_pml.py
@@ -49,5 +49,31 @@ print(f'norm_not_pml: {norm_not_pml}')
 
 # draw_surface(data_pml['x'][:1001], data_pml['y'], data_pml['z'][:, :1001] / norm_pml)
 # draw_surface(data_not_pml['x'][:1001], data_not_pml['y'], data_not_pml['z'][:, :1001] / norm_not_pml)
-draw_surface(data_not_pml['x'][:1001], data_not_pml['y'],
-             - data_pml['z'][:, :1001] / norm_pml + data_not_pml['z'][:, :1001] / norm_not_pml)
+# draw_surface(data_not_pml['x'][:1001], data_not_pml['y'],
+#              - data_pml['z'][:, :1001] / norm_pml + data_not_pml['z'][:, :1001] / norm_not_pml)
+
+
+fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+for i in range(len(data_not_pml['y'])):
+    axs[0].set_title('Разница')
+    axs[0].plot(data_not_pml['x'][:1001],
+                - data_pml['z'][i, :1001] / norm_pml + data_not_pml['z'][i, :1001] / norm_not_pml, color='blue')
+
+for i in range(len(data_not_pml['y'])):
+    axs[1].set_title('C PML')
+    axs[1].set_ylim([-0.005,0.02])
+    axs[1].plot(data_not_pml['x'][:1001],
+                data_pml['z'][i, :1001]/norm_pml, color='blue')
+
+for i in range(len(data_not_pml['y'])):
+    axs[2].set_title('Без PML')
+    axs[2].set_ylim([-0.005,0.02])
+    axs[2].plot(data_not_pml['x'][:1001],
+                data_not_pml['z'][i, :1001]/norm_not_pml, color='blue')
+
+plt.show()
+
+
+diff = data_not_pml['z'][:, :1001] / norm_not_pml - data_pml['z'][:, :1001] / norm_pml
+mse = np.mean(diff**2)
+print(f'MSE между с PML и без PML: {mse}')
