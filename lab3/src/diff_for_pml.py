9f36a358f7dac3241c86c61450f3b63af0e458d2
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-04-13 15:25:53 +0500

    Короче хз

--- lab3/src/diff_for_pml.py
+++ lab3/src/diff_for_pml.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 import matplotlib
 
@@ -53,27 +54,89 @@ print(f'norm_not_pml: {norm_not_pml}')
 #              - data_pml['z'][:, :1001] / norm_pml + data_not_pml['z'][:, :1001] / norm_not_pml)
 
 
-fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
-for i in range(len(data_not_pml['y'])):
-    axs[0].set_title('Разница')
-    axs[0].plot(data_not_pml['x'][:1001],
-                - data_pml['z'][i, :1001] / norm_pml + data_not_pml['z'][i, :1001] / norm_not_pml, color='blue')
+# fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+# for i in range(len(data_not_pml['y'])):
+#     axs[0].set_title('Not PML - PML')
+#     axs[0].plot(data_not_pml['x'][:1001],
+#                  - data_pml['z'][i, :1001] / norm_pml + data_not_pml['z'][i, :1001] / norm_not_pml, color='blue')
+#
+# for i in range(len(data_not_pml['y'])):
+#     axs[1].set_title('C PML')
+#     axs[1].set_ylim([-0.005,0.02])
+#     axs[1].plot(data_not_pml['x'][:1001],
+#                 data_pml['z'][i, :1001]/norm_pml, color='blue')
+#
+# for i in range(len(data_not_pml['y'])):
+#     axs[2].set_title('Без PML')
+#     axs[2].set_ylim([-0.005,0.02])
+#     axs[2].plot(data_not_pml['x'][:1001],
+#                 data_not_pml['z'][i, :1001]/norm_not_pml, color='blue')
+# plt.show()
 
-for i in range(len(data_not_pml['y'])):
-    axs[1].set_title('C PML')
-    axs[1].set_ylim([-0.005,0.02])
-    axs[1].plot(data_not_pml['x'][:1001],
-                data_pml['z'][i, :1001]/norm_pml, color='blue')
+# diff = data_not_pml['z'][:, :1001] / norm_not_pml - data_pml['z'][:, :1001] / norm_pml
+# mse = np.mean(diff**2)
+# print(f'MSE между с PML и без PML: {mse}')
 
-for i in range(len(data_not_pml['y'])):
-    axs[2].set_title('Без PML')
-    axs[2].set_ylim([-0.005,0.02])
-    axs[2].plot(data_not_pml['x'][:1001],
-                data_not_pml['z'][i, :1001]/norm_not_pml, color='blue')
+x = data_not_pml['x'][:1001]
+z_not_pml = data_not_pml['z'][:, :1001] / norm_not_pml
+z_pml = data_pml['z'][:, :1001] / norm_pml
+z_diff = -z_pml + z_not_pml
 
-plt.show()
+# Инициализируем фигуру
+fig = go.Figure()
 
+# Максимальная кратность
+max_step = 10
 
-diff = data_not_pml['z'][:, :1001] / norm_not_pml - data_pml['z'][:, :1001] / norm_pml
-mse = np.mean(diff**2)
-print(f'MSE между с PML и без PML: {mse}')
+# Добавим трассы для разных step (каждую n-ю линию)
+for step in range(1, max_step + 1):
+    visible = [False] * ((max_step) * 3)
+    traces = []
+
+    for i in range(0, len(z_not_pml), step):
+        trace1 = go.Scatter(x=x, y=z_not_pml[i], mode='lines',
+                            name='NOT PML' if i == 0 else None,
+                            line=dict(color='blue'),
+                            visible=(step == 1))
+        trace2 = go.Scatter(x=x, y=z_pml[i], mode='lines',
+                            name='PML' if i == 0 else None,
+                            line=dict(color='green', dash='dot'),
+                            visible=(step == 1))
+        trace3 = go.Scatter(x=x, y=z_diff[i], mode='lines',
+                            name='DIFF' if i == 0 else None,
+                            line=dict(color='red', dash='dash'),
+                            visible=(step == 1))
+        traces.extend([trace1, trace2, trace3])
+
+    fig.add_traces(traces)
+
+# Слайдерные шаги
+steps = []
+for s, step in enumerate(range(1, max_step + 1)):
+    visibility = []
+    for i in range(max_step):
+        # Скрываем все, кроме текущего шага
+        show = (i == s)
+        n_lines = len(range(0, len(z_not_pml), i + 1))
+        visibility.extend([show] * (n_lines * 3))
+    steps.append(dict(method="update",
+                      label=f"Каждую {step}-ю",
+                      args=[{"visible": visibility}]))
+
+# Добавим слайдер
+sliders = [dict(
+    active=0,
+    currentvalue={"prefix": "Кратность: "},
+    pad={"t": 50},
+    steps=steps
+)]
+
+fig.update_layout(
+    sliders=sliders,
+    title="Сравнение профилей: NOT PML, PML и DIFF",
+    xaxis_title="X",
+    yaxis_title="Амплитуда",
+    height=600
+)
+
+fig.show()
